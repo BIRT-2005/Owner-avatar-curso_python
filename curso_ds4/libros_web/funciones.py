7677a4c25879f8f9a0a5cbccf54fb5be9e848dc0
BIRT-2005 <bi0937151@gmail.com>
2025-03-06 14:32:23 -0700

    crea-_dcicionario() y busca_en_diccionario() sirven con 'title' y 'author'

--- curso_ds4/libros_web/funciones.py
+++ curso_ds4/libros_web/funciones.py
@@ -6,11 +6,34 @@ def lee_archivo_csv(archivo:str)->list:
     with open(archivo, "r", encoding='utf8') as f:
         return [x for x in csv.DictReader(f)]
 
-def crea_diccionario_titulos(lista:list)->dict:
+def crea_diccionario(lista:list,llave:str)->dict:
     ''' Crea un diccionario con los títulos como clave y el resto de los datos como valores '''
-    return {x['Titulo']: x for x in lista}
+    return {x[llave].lower(): x for x in lista}
+
+def busca_en_diccionario(diccionario, palabra)->list:
+    ''' Busca palabra en titulo de la lista de diccionarios '''
+    lista = []
+    palabra = palabra.lower()
+    for llave, libro in diccionario.items():
+        if palabra in llave.lower():
+            lista.append(libro)
+    return lista
+
+def busca_en_diccionario(diccionario:dict, palabra:str)->list:
+    ''' Busca la palabra en llave de la lista dicicionario '''
+    lista = []
+    palabra = palabra.lower()
+    for llave, libro in diccionario.items():
+        if palabra in llave.lower():
+            lista.append(libro)
+    return lista
 
 if __name__ == '__main__':
     archivo_csv = 'booklist2000.csv'
     lista_libros = lee_archivo_csv(archivo_csv)
-    diccionario_libros = crea_diccionario_titulos(lista_libros)
+    diccionario_libros = crea_diccionario(lista_libros, 'title')
+    resutaldo = busca_en_diccionario(diccionario_libros, 'rebels')
+    print(resutaldo)
+    diccionario_autores = crea_diccionario(lista_libros, 'author')
+    resutaldo = busca_en_diccionario(diccionario_autores, 'Sandra')
+    print(resutaldo)
